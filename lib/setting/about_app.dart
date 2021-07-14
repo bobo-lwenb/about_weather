2f8466698d1590996c0ad05fef0fed82473e042d
liuwb <eryuehao@gmail.com>
2021-07-14 22:50:11 +0800

    Update about_app.dart

--- lib/setting/about_app.dart
+++ lib/setting/about_app.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:about_weather/tool_box/fields.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -10,36 +9,43 @@ class AboutApp extends StatefulWidget {
   _AboutAppState createState() => _AboutAppState();
 }
 
-class _AboutAppState extends State<AboutApp> {
+class _AboutAppState extends State<AboutApp>
+    with SingleTickerProviderStateMixin {
   String _appName = "";
   String _appVersion = "";
 
+  ValueNotifier _valueNotifier = ValueNotifier<double>(0);
+  late AnimationController _controller;
+
   @override
   void initState() {
-    info();
     super.initState();
+    _info();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 800), vsync: this);
+    _controller.addListener(() {
+      _valueNotifier.value = (1 - _controller.value) * _valueNotifier.value;
+      if (_controller.isCompleted) _controller.reset();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        shadowColor: Colors.transparent,
-        elevation: 0,
-        backgroundColor: isDark(context) ? Colors.grey[850] : Colors.white10,
-      ),
-      body: Center(
-        child: Column(
+    Column column = Column(
+      children: [
+        SizedBox(height: 32),
+        Text(
+          AppLocalizations.of(context)!.welcome,
+          style: TextStyle(fontSize: 22),
+        ),
+        SizedBox(height: 150),
+        Text(
+          _appName,
+          style: TextStyle(fontSize: 42),
+        ),
+        SizedBox(height: 150),
+        Column(
           children: [
-            SizedBox(height: 16),
-            Text(AppLocalizations.of(context)!.welcome,
-                style: TextStyle(fontSize: 22)),
-            Container(
-              height: 255,
-              child: Center(
-                child: Text(_appName, style: TextStyle(fontSize: 42)),
-              ),
-            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -51,11 +57,55 @@ class _AboutAppState extends State<AboutApp> {
             Text(AppLocalizations.of(context)!.developer),
           ],
         ),
+      ],
+    );
+    ValueListenableBuilder builder = ValueListenableBuilder(
+      valueListenable: _valueNotifier,
+      builder: (context, value, child) {
+        return Positioned(
+          top: value as double,
+          child: child!,
+        );
+      },
+      child: column,
+    );
+    GestureDetector detector = GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onVerticalDragUpdate: (details) {
+        _valueNotifier.value += details.delta.dy / 2;
+      },
+      onVerticalDragEnd: (details) {
+        _controller.forward();
+      },
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          builder,
+        ],
+      ),
+    );
+    return Theme(
+      data: ThemeData.dark(),
+      child: Scaffold(
+        appBar: AppBar(
+          shadowColor: Colors.transparent,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Text(AppLocalizations.of(context)!.aboutApp),
+        ),
+        backgroundColor: Colors.deepOrange[400],
+        body: detector,
       ),
     );
   }
 
-  void info() async {
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _info() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     _appName = packageInfo.appName;
     _appVersion = packageInfo.version;
